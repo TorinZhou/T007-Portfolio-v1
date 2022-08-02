97ffa9ab0cb71a440b7fbc71126136a00d49e7e7
Torin Zhou <torinzhou@gmail.com>
2022-08-02 16:41:33 +0800

    8.2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import Navbar from "./components/Navbar";
-import Logo from "./assets/logo-without-margins.png";
-import { FaBars, Fatimes } from "react-icons/fa";
 
 function App() {
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,22 +5,22 @@ import { FaBars, Fatimes } from "react-icons/fa";
 const Navbar = () => {
   return (
     <>
-      <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div className="fixed flex h-[80px] w-full items-center justify-between bg-[#0a192f] px-4 text-gray-300">
         <div>
-          <img src={Logo} style={{ width: "150px" }}></img>
+          <img src={Logo} alt="logo" style={{ width: "150px" }}></img>
         </div>
         {/* Menu */}
-        <div className="hidden md:flex">
-          <ul className="hidden md:flex">
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Work</li>
-            <li>Contact</li>
-          </ul>
-        </div>
+
+        <ul className="hidden md:flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+
         {/* Hamburger */}
-        <div className="hidden">
+        <div className="md:hidden">
           <FaBars />
         </div>
         {/* Mobile Menu */}
