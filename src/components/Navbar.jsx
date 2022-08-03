233315aeb026c24a4beb2582b304f1f0f2416f5a
Torin Zhou <torinzhou@gmail.com>
2022-08-03 16:52:10 +0800

    8.3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/logo-without-margins.png";
 import { FaBars, Fatimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [menuIsShown, setMenuIsShown] = useState(false);
+  const toggleMenu = () => setMenuIsShown((menuIsShown) => !menuIsShown);
+
   return (
     <>
       <div className="fixed flex h-[80px] w-full items-center justify-between bg-[#0a192f] px-4 text-gray-300">
@@ -10,7 +13,6 @@ const Navbar = () => {
           <img src={Logo} alt="logo" style={{ width: "150px" }}></img>
         </div>
         {/* Menu */}
-
         <ul className="hidden md:flex">
           <li>Home</li>
           <li>About</li>
@@ -18,23 +20,26 @@ const Navbar = () => {
           <li>Work</li>
           <li>Contact</li>
         </ul>
-
         {/* Hamburger */}
-        <div className="md:hidden">
+        <div className="z-10 md:hidden" onClick={toggleMenu}>
           <FaBars />
         </div>
         {/* Mobile Menu */}
-        <div className="hidden">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Work</li>
-            <li>Contact</li>
-          </ul>
-        </div>
+        <ul
+          className={
+            !menuIsShown
+              ? "hidden"
+              : "absolute top-0 left-0 flex h-screen w-full flex-col items-center justify-center bg-[#0a192f]"
+          }
+        >
+          <li className="py-6 text-4xl">Home</li>
+          <li className="py-6 text-4xl">About</li>
+          <li className="py-6 text-4xl">Skills</li>
+          <li className="py-6 text-4xl">Work</li>
+          <li className="py-6 text-4xl">Contact</li>
+        </ul>
         {/* Social Icons */}
-        <div className="hidden"></div>
+        {/* {menuIsShown && <div className="hidden"></div>} */}
       </div>
     </>
   );
